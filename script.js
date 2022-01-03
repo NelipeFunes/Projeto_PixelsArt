1aa2c6a16e967f75d75d4e632125c9503dea2a86
NelipeFunes <miotarestaurante2003@gmail.com>
2022-01-03 11:03:01 -0300

    Ue

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//feito na ajuda dos colegas na sala de estudos
+//Feito na ajuda dos amigãozoes na sala de estudos <3
 let linhas = 5;
 
 for(let index = 0; index<linhas; index+=1){
@@ -8,7 +8,24 @@ for(let index = 0; index<linhas; index+=1){
 
     for(i = 0; i<linhas;i+=1){
         let coluns = document.createElement('div');
-        coluns.classList.add('pixel')
-        lines.appendChild(coluns)
+        coluns.classList.add('pixel');
+        lines.appendChild(coluns);
     }
+}
+
+let black = document.getElementById('preto');
+let red = document.getElementById('vermelho');
+let green = document.getElementById('verde');
+let blue = document.getElementById('azul');
+
+black.addEventListener('click', changeColor)
+red.addEventListener('click', changeColor);;
+green.addEventListener('click', changeColor);
+blue.addEventListener('click', changeColor);
+
+function changeColor(){
+   for(let i = 0; i < 4;i+=1)
+   let colorSelect = event.currentTarget;
+   colorSelect.classList.add('selected');
+   
 }
